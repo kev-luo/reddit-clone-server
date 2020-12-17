ea6b00208d3844692c166c6676b6050f38dd7f21
Kevin Luo <kvn.luo@gmail.com>
2020-12-17 12:32:53 -0500

    set up migration config

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,8 +1,13 @@
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import { MikroORM } from "@mikro-orm/core";
+import path from "path";
 
 export default {
+  migrations: {
+    path: path.join(__dirname, './migrations'), // returns absolute path to the folder with migrations. 
+    pattern: /^[\w-]+\d+\.[tj]s$/, // regex pattern for the migration files
+  },
   entities: [Post], // db tables
   dbName: 'redditclone',
   type: 'postgresql',
