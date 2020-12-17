cec3eba1a42bbc06b0033d61df799a324f8eb2a8
Kevin Luo <kvn.luo@gmail.com>
2020-12-17 12:08:44 -0500

    ORM set up

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('hello orld');
+import { MikroORM } from "@mikro-orm/core";
+
+const main = async() => {
+  const orm = MikroORM.init({
+    dbName: 'redditclone',
+    type: 'postgresql',
+    debug: process.env.NODE_ENV !== 'production',
+  });
+}
+
+main();
