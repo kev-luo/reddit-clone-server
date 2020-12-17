d65a43d1059451d632ac71b09d6b8933cef5e4c3
Kevin Luo <kvn.luo@gmail.com>
2020-12-17 14:34:47 -0500

    hash password with argon2 and save to db

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,6 +1,7 @@
-import { User } from "src/entities/User";
+import { User } from "../entities/User";
 import { MyContext } from "src/types";
 import { Arg, Ctx, Field, InputType, Mutation, Resolver } from "type-graphql";
+import argon2 from "argon2";
 
 // input types for args
 @InputType()
@@ -16,10 +17,12 @@ class RegisterInput {
 export class UserResolver {
   @Mutation(() => User)
   async register(
-    @Arg('options') options: RegisterInput ,
+    // typeGraphQL infers the type so we don't have to add () => RegisterInput
+    @Arg('options') options: RegisterInput,
     @Ctx() ctx: MyContext
   ): Promise<User> {
-    const user = await ctx.em.create(User, { username: options.username})
+    const hashedPassword = await argon2.hash(options.password);
+    const user = await ctx.em.create(User, { username: options.username, password: hashedPassword })
     await ctx.em.persistAndFlush(user);
     return user;
   }
