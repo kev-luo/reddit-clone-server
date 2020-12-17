89873dfbad18e809959af509a44ece70cad12b38
Kevin Luo <kvn.luo@gmail.com>
2020-12-17 12:29:39 -0500

    create mikro orm config file

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,22 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import microConfig from "./mikro-orm.config";
 
-const main = async() => {
-  const orm = MikroORM.init({
-    entities: [], // db tables
-    dbName: 'redditclone',
-    type: 'postgresql',
-    debug: !__prod__,
-  });
+const main = async () => {
+  const orm = await MikroORM.init(microConfig);
+
+  // create instance of post
+  const post = orm.em.create(Post, { title: "my first post" })
+  // save to database
+  await orm.em.persistAndFlush(post);
+
+  console.log("----------------sql2---------------------")
+
+  // alternatively we can combine the two commands above by doing:
+  await orm.em.nativeInsert(Post, { title: "my second post" })
 }
 
-main();
+main().catch(err => {
+  console.error(err);
+});
